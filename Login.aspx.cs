e863fcba23c3c2e069e312253e9b331acff343bb
Hardeep5465 <33686995+Hardeep5465@users.noreply.github.com>
2017-12-20 11:50:38 +0530

    mysql code commented

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -7,19 +7,19 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.DataVisualization.Charting;
 using System.IO;
-using System.Net;
-using System.Net.Mail;
-using System.Data;
+//using System.Net;
+//using System.Net.Mail;
+//using System.Data;
 using System.Drawing;
 using System.Configuration;
-using MySql.Data.MySqlClient;
+//using MySql.Data.MySqlClient;
 using System.Web.Security;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 
 public partial class Login : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+ /*   protected void Page_Load(object sender, EventArgs e)
     {
 
         Response.Cache.SetCacheability(HttpCacheability.NoCache);
@@ -2398,5 +2398,5 @@ public partial class Login : System.Web.UI.Page
     {
         personlist.SelectedValue = "";
     }
-
+*/
 }
